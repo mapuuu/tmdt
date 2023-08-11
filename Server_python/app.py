51a9af9f094448c635261fb345ce9b448e1aac39
mapuuu <phutoannguyen2271@gmail.com>
2023-08-11 15:25:01 +0700

    update python

---
+++ Server_python/app.py
@@ -0,0 +1,156 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+import numpy as np
+from model import Model
+import pymongo
+from bson import ObjectId, json_util
+from sklearn.metrics.pairwise import cosine_similarity
+import cv2
+import json
+app = Flask(__name__)
+
+CORS(app, supports_credentials=True, origins='http://localhost:3000')
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+# mongo_connection_string = "mongodb+srv://commerce:commerce@cluster0.lygmgn5.mongodb.net/commerce?retryWrites=true&w=majority"
+mongo_connection_string = "mongodb://localhost:27017/"
+
+
+# Kết nối với MongoDB Atlas
+client = pymongo.MongoClient(mongo_connection_string)
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+# database = client["commerce"]
+database = client["test"]
+products = pd.DataFrame(list(database["products"].find()))
+products.sort_values('rate', inplace=True)
+popular_products = products[products['numberOfReviews'] >=50]
+final_df = products[products['_id'].isin(popular_products['_id'])]
+pt = final_df.pivot_table(index='_id', columns='category', values='numberOfReviews')
+print(pt.index)
+pt.replace(np.nan, 0, inplace=True)
+similarity = cosine_similarity(pt)
+def get_top_rated_products(product_id):
+    if ObjectId(product_id) not in products['_id'].unique():
+        print(f"Product ID {ObjectId(product_id)} không tồn tại trong danh sách sản phẩm.")
+        return None
+    target_product = products[products['_id'] == ObjectId(product_id)]
+    target_category = target_product['category'].iloc[0]
+    filtered_products = products[(products['category'] == target_category) & (products['numberOfReviews'] > 30)]
+    filtered_products = filtered_products[filtered_products['_id'] != product_id]
+    top_rated_products = filtered_products.sort_values('rate', ascending=False)
+    top_10_rated_products = top_rated_products.head(10)
+    return top_10_rated_products
+
+
+#  kiểm tra xem item có phải là sản phẩm phổ biến hay không, nếu phải thì tính toán xem độ tương đồng là bn
+#  x[1] là giá trị tương đồng chạy từ -1;1
+#  danh sách recommend sẽ trả về các sp có độ tương đồng cao nhất từ trên xuống dưới , cho dù là sp phổ biến nhưng chưa chắc
+#  tương đồng
+#  có trường hợp chẳng đưa ra được recommend vì sp không có độ tương đồng
+def recommedation_system(item):
+    recommended_products_list = []
+    if ObjectId(item) in pt.index:
+        index = np.where(pt.index == ObjectId(item))[0][0]
+        similarity_products = sorted(list(enumerate(similarity[index])), key=lambda x: x[1], reverse=True)[1:11]
+        category_of_item = products.loc[products['_id'] == ObjectId(item), 'category'].values[0]
+        for i in similarity_products:
+            product_ID = pt.index[i[0]]
+            if product_ID != ObjectId(item):
+                product_info = products.loc[products['_id'] == product_ID]
+                if len(product_info) > 0:
+                    category_of_product = product_info['category'].values[0]
+                    if category_of_product == category_of_item:
+                        product_entry= {
+                            "_id": str(product_info['_id'].values[0]),
+                            "category": product_info['category'].values[0],
+                            "name": product_info['name'].values[0],
+                            "description": product_info['description'].values[0],
+                            "rate": float(product_info['rate'].values[0]),
+                            "images": product_info['images'].values[0],
+                            "quantity": int(product_info['quantity'].values[0]),
+                            "price": int(product_info['price'].values[0])
+                        }
+                        if product_entry not in recommended_products_list:
+                            recommended_products_list.append(product_entry)
+                        # recommended_products_list = json.dumps(recommended_products_list, indent=4)
+    if len(recommended_products_list) < 10:
+        top_rated_products = get_top_rated_products(ObjectId(item)).head(10 - len(recommended_products_list))
+        top_rated_products_list = top_rated_products[
+            ['_id', 'category', 'name', 'description', 'rate', 'images', 'quantity', 'price']].to_dict(orient='records')
+        for product in top_rated_products_list:
+            product_entry = {
+                "_id": str(product["_id"]),
+                "category": product["category"],
+                "name": product["name"],
+                "description": product["description"],
+                "rate": float(product["rate"]),
+                "images": product["images"],
+                "quantity": int(product["quantity"]),
+                "price": int(product["price"])
+            }
+            if product_entry not in recommended_products_list:
+                recommended_products_list.append(product_entry)
+    if len(recommended_products_list) < 10:
+        product_info = products.loc[products['_id'] == ObjectId(item)]
+        if not product_info.empty:
+            category = product_info['category'].values[0]
+            category_product = products.loc[(products['category'] == category) & (products['_id'] != ObjectId(item))]
+            category_product = category_product.head(10 - len(recommended_products_list))
+            for _, product in category_product.iterrows():
+                product_entry={
+                    "_id": str(product['_id']),
+                    "category": product['category'],
+                    "name": product['name'],
+                    "description": product['description'],
+                    "rate": float(product['rate']),
+                    "images": product['images'],
+                    "quantity": int(product['quantity']),
+                    "price": int(product['price'])
+                }
+                if product_entry not in recommended_products_list:
+                    recommended_products_list.append(product_entry)
+    recommended_products_list = json.dumps(recommended_products_list, indent=4)
+    print(recommended_products_list)
+    return recommended_products_list
+
+@app.route('/')
+def home():
+    return "Welcome to API Server predict image!!!"
+
+@app.route('/test', methods=['POST'])
+def test():
+    try:
+        data = request.get_json()['id']
+        print(data)
+        return jsonify(data)
+    except Exception as e:
+        print(e)
+
+@app.route('/process-image', methods=['POST'])
+def process_image():
+    try:
+        # Nhận dữ liệu hình ảnh từ microservice Node.js
+        image_data = request.files["file"]
+        file_bytes = np.asarray(bytearray(image_data.read()), dtype=np.uint8)
+        image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        model = Model()
+
+        image_data = model.predict(image)
+        # Trả về kết quả xử lý ảnh dưới dạng JSON
+        return jsonify(image_data), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'Lỗi khi xử lý hình ảnh'}), 500
+    
+@app.route('/receive', methods=['POST', 'GET', 'PUT'])
+def receive():
+    if request.method == 'POST':
+        data = request.json.get("obj_id")
+        file_data = recommedation_system(data)
+        return file_data
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ Server_python/model.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras import layers
+from keras.models import Sequential, load_model
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+import cv2
+import pathlib
+
+class Model:
+    
+    def __init__(self):
+        self.model = None
+        
+    # def predict(self, image):
+    #     self.model = load_model('../server/model/test/Demo_anhCam_anhNong.model')
+        
+    #     if self.model is None:
+    #         self.train_model()
+        
+    #     class_names = ["Anh cam", "Anh nong", "Anh thong thuong"]
+    #     img_resized = cv2.resize(image, (180, 180))
+        
+    #     img_4d = np.array(img_resized).reshape(-1, 180, 180, 3)
+    #     prediction = self.model.predict(img_4d)
+    #     return class_names[np.argmax(prediction)]
+    
+    def predict(self, image):
+        self.model = load_model('./train_model.h5')
+        
+        img = cv2.resize(image, (256, 256))
+
+        i = img_to_array(img)
+
+        i = preprocess_input(i)
+
+        input_arr = np.array([i])
+
+        input_arr.shape
+        
+        pred = np.argmax(self.model.predict(input_arr))
+        
+        if pred == 0:
+            print("Anh cam")
+            pred = 'Anh cam'
+        if pred == 1:
+            pred = 'Anh nong'
+            print("Anh nong")
+        if pred == 2:
+            pred = 'Balo'
+            print("Balo")
+        if pred == 3:
+            pred = 'Book'
+            print("Book")
+        if pred == 4:
+            pred = 'Laptop'
+            print("Laptop")
+        if pred == 5:
+            pred = 'Smartphone'
+            print("Smartphone")
+        if pred == 6:
+            pred = 'Vali'
+            print("Vali")
+        if pred == 7:
+            pred = 'Watch'
+            print("Watch")
+        return pred
+        
+    
+    def train_model(self):
+        datadir = "../server/data/Training"
+        train_ds = ["Banana", "Bycircle", "CD", "Calculator", "Computer-Mouse"]
+        data_dir = pathlib.Path(datadir)
+        
+        img_height, img_width = 180, 180
+        batch_size = 64
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size)
+        
+        val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size)
+        
+        num_classes = 5
+
+        self.model = Sequential([
+            layers.experimental.preprocessing.Rescaling(
+                1./255, input_shape=(img_height, img_width, 3)),
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(128, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(512, activation='relu'),
+            layers.Dense(num_classes, activation='softmax')
+        ])
+
+        self.model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy']
+        )
+        epochs = 10
+        history = self.model.fit(
+            train_ds,
+            #steps_per_epoch=5,
+            epochs=epochs,
+            verbose=1,
+            validation_data=val_ds,
+            validation_steps=1
+        )
+        
+        self.model.save('../server/data/v1/Object_Detection_Prj_obj.model')
